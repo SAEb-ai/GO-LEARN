7276c57045fbf883fd0edec653fda65efb5bf4e1
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2022-05-22 15:44:48 +0530

    Go program to print my name by taking input
    Signed-off-by: Md Shahbaz Alam <shahbazalam75508@gmail.com>

---
+++ MyName/printInputName.go
@@ -0,0 +1,16 @@
+//Go program to print my name by taking input
+package main
+
+//Importing the Format Package
+import "fmt"
+
+//Entry point of Go File
+func main() {
+
+	var name string
+	//Taking input of my name
+	fmt.Scan(&name)
+	//Printing name
+	fmt.Println("My Name is", name)
+
+}

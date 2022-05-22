556f0935c0fd24844ce91517de492225313c3084
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2022-05-22 13:11:51 +0530

    Go program to print my name stored in a variable
    Signed-off-by: Md Shahbaz Alam <shahbazalam75508@gmail.com>

---
+++ MyName/printVarName.go
@@ -0,0 +1,14 @@
+//Go program to print my name stored in a variable
+package main
+
+//Importing the Format Package
+import "fmt"
+
+//Entry point of Go File
+func main() {
+
+	var myName = "Md Shahbaz Alam"
+	//Printing My Name stored in myName
+	fmt.Println("My Name is", myName);
+
+}
